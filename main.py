9e7612ecd0a7e0c7bfb4839e9e7983ef8f7369fc
murchie85 <murchie85@gmail.com>
2023-02-14 22:03:09 +0000

    adding readme

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import numpy as np
 
-
-
 # Prepare your data
 # Generate some data
 X = np.array([[0.1, 0.2, 0.3],
-			  [0.4, 0.5, 0.6],
-			  [0.7, 0.8, 0.9]])
+              [0.4, 0.5, 0.6],
+              [0.7, 0.8, 0.9]])
 y = np.array([[0.4],
-			  [0.7],
-			  [1.0]])
-
+              [0.7],
+              [1.0]])
 
 # Define the RNN model
 # Set the number of units in the hidden layer
@@ -24,43 +21,35 @@ Why = np.random.randn(1, num_hidden_units)
 bh = np.zeros((num_hidden_units, 1))
 by = np.zeros((1, 1))
 
-
-
-
 # Define the forward pass function to return the output sequence and final hidden state
 def rnn_forward(X, Wxh, Whh, Why, bh, by):
-	# Initialize the hidden state to zero
-	h = np.zeros((num_hidden_units, 1))
+    # Initialize the hidden state to zero
+    h = np.zeros((num_hidden_units, 1))
 
-	# Initialize the output sequence
-	Y = np.zeros((X.shape[0], 1))
+    # Initialize the output sequence
+    Y = np.zeros((X.shape[0], 1))
 
-	# Loop over each time step in the input sequence
-	for t in range(X.shape[0]):
-		# Get the current input and reshape it to a column vector
-		x = X[t].reshape((-1, 1))
+    # Loop over each time step in the input sequence
+    for t in range(X.shape[0]):
+        # Get the current input and reshape it to a column vector
+        x = X[t].reshape((-1, 1))
 
-		# Compute the new hidden state
-		h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
+        # Compute the new hidden state
+        h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
 
-		# Compute the output at the current time step
-		y = np.dot(Why, h) + by
+        # Compute the output at the current time step
+        y = np.dot(Why, h) + by
 
-		# Add the output to the output sequence
-		Y[t] = y
+        # Add the output to the output sequence
+        Y[t] = y
 
-	return Y, h
+    return Y, h
+
 
 
 # Define the loss function
 def mse_loss(Y, y_true):
-	return np.mean((Y - y_true) ** 2)
-
-
-
-# Set the learning rate and number of epochs
-learning_rate = 0.01
-num_epochs = 100
+    return np.mean((Y - y_true) ** 2)
 
 # Set the learning rate and number of epochs
 learning_rate = 0.01
@@ -68,48 +57,52 @@ num_epochs = 100
 
 # Train the RNN
 for epoch in range(num_epochs):
-	# Perform the forward pass
-	Y_pred, h = rnn_forward(X, Wxh, Whh, Why, bh, by)
+    # Perform the forward pass
+    Y_pred, h = rnn_forward(X, Wxh, Whh, Why, bh, by)
 
-	# Compute the loss
-	loss = mse_loss(Y_pred, y)
+    # Compute the loss
+    loss = mse_loss(Y_pred, y)
 
-	# Compute the gradients using backpropagation
-	dLdY = 2 * (Y_pred - y)
-	dLdWhy = np.dot(dLdY.T, h.T)
-	dLdh = np.dot(Why.T, dLdY) * (1 - h ** 2)
-	dLdWhh = np.dot(dLdZ.T, h.T)
-	dLdWxh = np.dot(dLdh, x.T)
-	dLdbh = np.sum(dLdh, axis=1, keepdims=True)
-	dLdby = np.sum(dLdY, axis=0, keepdims=True)
+    # Compute the gradients using backpropagation
+    dLdY = 2 * (Y_pred - y)
+    dLdWhy = np.dot(dLdY.T, h.T)
+    dLdh = np.dot(dLdY, Why)
+    dLdZ = dLdh * (1 - h ** 2)
+    dLdWhh = np.dot(dLdZ.T, h.T)
+    dLdWxh = np.dot(dLdZ.T, X).T
+    dLdbh = np.sum(dLdZ, axis=1, keepdims=True)
+    dLdby = np.sum(dLdY, axis=0, keepdims=True)
 
+    # Update the weights and biases using SGD
+    Wxh -= learning_rate * dLdWxh
+    Whh -= learning_rate * dLdWhh
+    Why -= learning_rate * dLdWhy.T
+    bh -= learning_rate * dLdbh
+    by -= learning_rate * dLdby
 
-	# Update the weights and biases using SGD
-	Wxh -= learning_rate * dLdWxh.T
-	Whh -= learning_rate * dLdWhh.T
-	Why -= learning_rate * dLdWhy.T
-	bh -= learning_rate * dLdbh
-	by -= learning_rate * dLdby
-
-	# Print the loss every 10 epochs
-	if epoch % 10 == 0:
-		print('Epoch %d, Loss: %f' % (epoch, loss))
-
+    # Print the loss every 10 epochs
+    if epoch % 10 == 0:
+        print('Epoch %d, Loss: %f' % (epoch, loss))
 
 # Generate a new sequence
+seq_len = X.shape[0]
+h = np.zeros((num_hidden_units, 1))
+Y_pred, _ = rnn_forward(X, Wxh, Whh, Why, bh, by)
+
 for t in range(seq_len, seq_len + 10):
-	# Get the current input and reshape it to a column vector
-	x = np.array([[X[t - 1]]])
+    # Get the current input and reshape it to a column vector
+    x = np.array([[X[t - 1]]])
 
-	# Compute the new hidden state
-	h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
+    # Compute the new hidden state
+    h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
 
-	# Compute the output at the current time step
-	y = np.dot(Why, h) + by
+    # Compute the output at the current time step
+    y = np.dot(Why, h) + by
 
-	# Add the output to the output sequence
-	Y_pred = np.vstack((Y_pred, y))
+    # Add the output to the output sequence
+    Y_pred = np.vstack((Y_pred, y))
 
 # Print the output sequence
 print('Output sequence:')
 print(Y_pred)
+
